28ab73443983c5d3c03b59905b1e202439e6064c
fatima-tehreem09 <tehreem.hashmi007@gmail.com>
2025-10-17 16:40:29 +0500

    Added get_it DI

---
+++ lib/src/core/di/locator.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../data/quiz_api.dart';
+import '../../data/quiz_repository.dart';
+import '../network/dio_client.dart';
+import '../session/user_session.dart';
+import '../theme/theme_cubit.dart';
+
+final GetIt serviceLocator = GetIt.instance;
+
+Future<void> setupServiceLocator() async {
+  serviceLocator.registerLazySingleton<Dio>(() => createDioClient());
+
+  serviceLocator
+      .registerLazySingleton<QuizApi>(() => QuizApi(serviceLocator<Dio>()));
+
+  serviceLocator.registerLazySingleton<QuizRepository>(
+      () => QuizRepositoryImpl(serviceLocator<QuizApi>()));
+
+  serviceLocator.registerLazySingleton<UserSession>(() => UserSession());
+
+  serviceLocator.registerLazySingleton<ThemeCubit>(() => ThemeCubit());
+}
